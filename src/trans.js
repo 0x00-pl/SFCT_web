bd8dd19a65595f617a7b512e77284df7e4498805
0x00-pl <0x00.pl@gmail.com>
2016-12-08 23:39:08 +0800

    add more code

--- src/trans.js
+++ src/trans.js
@@ -1,4 +1,5 @@
 let fs = require("fs");
+let path = require("path");
 
 
 function destruct_text(text){
@@ -21,10 +22,29 @@ function trans_text(text){
 }
 
 
-function trans_file(src_path. dst_path){
+function trans_file(src_path, dst_path){
+    src_path = path.normalize(src_path);
+    dst_path = path.normalize(dst_path);
+
     let text = fs.readFileSync(src_path, {encoding:"utf-8"});
     if(text == null){return "Cannot read file:"+src_path;}
     let transed_text = trans_text(text);
     fs.writeFIleSync(dst_path, transed_text, {encoding:"utf-8"});
 }
+
+function trans_vfile_dir(src_dir, dst_dir){
+    src_dir = path.normalize(src_dir);
+    dst_dir = path.normalize(dst_dir);
+    fs.mkdirSync(dst_dir);
+
+    let files = fs.readdirSync(src_dir);
+    let vfiles = files.filter(x => x.endswith(".v"), {encoding:"utf-8"});
+    vfiles.forEach(function(v){
+        trans_file(path.join(src_dir, v), path.join(dst_dir, v));
+    });
+}
+
+module.exports = {
+    trans_vfile_dir
+};
 
