00db9a2d9bf61c7a0e70b90d119793dad9eaa385
0x00-pl <0x00.pl@gmail.com>
2016-12-27 22:28:14 +0800

    close #8.

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ let server = express();
 
 server.use(app);
 
-server.listen(8888, function(){
-    console.log("listening: 8888");
+server.listen(80, function(){
+    console.log("listening: 80");
 });
 
 
